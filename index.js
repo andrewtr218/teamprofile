d88389584c24581ef5f731bad9f9ca02bc30b147
andrewtr218 <andrewtr218@gmail.com>
2020-01-21 08:53:03 -0600

    added templating for manager card and removed ability to have more than one manager

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const Engineer = require("./lib/classes/engineer.js");
 const Intern = require("./lib/classes/intern.js")
 const jest = require("jest");
 const inquirer = require("inquirer");
-
+const fs = require("fs");
 
 const i = 0;
+const managerList = [];
 
 
 inquirer.prompt([{
+        type: "list",
+        name: "Job Title",
+        choices: ["Manager", "Engineer", "Intern"]
+    },
+    {
         type: "input",
         name: "employee name",
         message: "What is the employees name?"
@@ -20,81 +26,125 @@ inquirer.prompt([{
         message: "What is the employees ID number?"
     },
     {
-        type: "list",
-        name: "Job Title",
-        choices: ["Manager", "Engineer", "Intern"]
-    },
-    {
         type: "input",
         name: "email",
         message: "What is the employees email address?"
     }
 ])
     .then(answers => {
-        var employee = new Employee (answers["employee name"], answers["id"], answers["Job Title"], answers["email"]);
+        const employee = new Employee (answers["employee name"], answers["id"], answers["Job Title"], answers["email"]);
         // console.log(employee);
+
         if(answers["Job Title"]==="Manager"){
-            const final = constManager(employee);
-                return final
+            inquirer.prompt([{
+                type: "input",
+                name: "Office Number",
+                message: "What is this managers's office number?"
+            },
+            ]).then(answers => {
+                const manager = new Manager (answers["Office Number"]);
+                manager.name = employee.name;
+                manager.id = employee.id;
+                manager.title = "Manager";
+                manager.email = employee.email;
+                if(managerList.length > 0){
+                    console.log("Too Many Managers");
+                }else{
+                    managerList.push(manager);
+                    console.log(managerList);
+                }
+                
+                
+            })
         }else if(answers["Job Title"]==="Engineer"){
-            const final = constEngineer(employee);
-            return final
+            inquirer.prompt([{
+                type: "input",
+                name: "GitHub",
+                message: "What is this engineer's GitHub Username?"
+            }
+            ]).then(answers => {
+                const engineer = new Engineer (answers["GitHub"]);
+                engineer.name = employee.name;
+                engineer.id = employee.id;
+                engineer.title = "Engineer"
+                engineer.email = employee.email;
+                    // $.ajax{
+                    //     engineer
+                    // }
+                
+            })
         }else if(answers["Job Title"]==="Intern"){
-            const final = constIntern(employee);
-                return final
+            inquirer.prompt([{
+                type: "input",
+                name: "school",
+                message: "What is this interns school of origin?"
+            }
+            ]).then(answers => {
+                const intern = new Intern (answers["school"]);
+                intern.name = employee.name;
+                intern.id = employee.id;
+                intern.title = "Intern"
+                intern.email = employee.email;
+                // console.log(intern);
+
+            })
         }else{
             console.log("error")
         }
-    }).then(final=>{console.log(final)})
+    })
 
-function constManager(employee){
-        inquirer.prompt([{
-            type: "input",
-            name: "Office Number",
-            message: "What is this managers's office number?"
-        }])
-        .then(answers => {
-            const manager = new Manager (answers["Office Number"]);
-            manager.name = employee.name;
-            manager.id = employee.id;
-            manager.title = "Manager"
-            manager.email = employee.email;
-            console.log(manager);
-            return manager;
-        })
-    }
-function constEngineer(employee){
-        inquirer.prompt([{
-            type: "input",
-            name: "GitHub",
-            message: "What is this engineer's GitHub Username?"
-        }])
-        .then(answers => {
-            const engineer = new Engineer (answers["GitHub"]);
-            engineer.name = employee.name;
-            engineer.id = employee.id;
-            engineer.title = "Engineer"
-            engineer.email = employee.email;
-            console.log(engineer);
-            return engineer;
-        })
-    }
-function constIntern(employee){
-        inquirer.prompt([{
-            type: "input",
-            name: "school",
-            message: "What is this interns school of origin?"
-        }])
-        .then(answers => {
-            const intern = new Intern (answers["school"]);
-            intern.name = employee.name;
-            intern.id = employee.id;
-            intern.title = "Intern"
-            intern.email = employee.email;
-            console.log(intern);
-            return intern;
-        })
-    }
+// function constManager(employee){
+//         inquirer.prompt([{
+//             type: "input",
+//             name: "Office Number",
+//             message: "What is this managers's office number?"
+//         },
+//         ]).then(answers => {
+//             const manager = new Manager (answers["Office Number"]);
+//             manager.name = employee.name;
+//             manager.id = employee.id;
+//             manager.title = "Manager"
+//             manager.email = employee.email;
+//             // console.log(manager);
+//             return manager;
+//         })
+//         };
+// function constEngineer(employee){
+//         inquirer.prompt([{
+//             type: "input",
+//             name: "GitHub",
+//             message: "What is this engineer's GitHub Username?"
+//         }
+//         ]).then(answers => {
+//             const engineer = new Engineer (answers["GitHub"]);
+//             engineer.name = employee.name;
+//             engineer.id = employee.id;
+//             engineer.title = "Engineer"
+//             engineer.email = employee.email;
+//             // console.log(engineer);
+//             return engineer;
+//         })
+//     };
+// function constIntern(employee){
+//         inquirer.prompt([{
+//             type: "input",
+//             name: "school",
+//             message: "What is this interns school of origin?"
+//         }
+//         ]).then(answers => {
+//             const intern = new Intern (answers["school"]);
+//             intern.name = employee.name;
+//             intern.id = employee.id;
+//             intern.title = "Intern"
+//             intern.email = employee.email;
+//             // console.log(intern);
+//             return intern;
+//         })
+//     };
+
+// function cardIntern(intern){
+
+// }
     // .prompt([{
     //     type: "list",
     //     name: "Job Title",
