242628ebfe8ee0c842ba51d959a57d2c83a631a2
andrewtr218 <andrewtr218@gmail.com>
2020-01-21 17:57:43 -0600

    completed basic assignment with full functionality

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Takes user input from the command line to generate profiles for different employees. Application built from scratch as the developer was not aware files were provided.
 
 Known Bugs:
-1. Information gained in split function not asyncing correctly, acceptable bug, pressing up re-prompts the user for information
 
 TODO:
+1. Create function which renders cards for all employees in the 3 arrays
 1. Render Manager to Manager Page
 2. Render Engineers to Engineers Page
 3. Render Interns to Interns Page
--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const fs = require("fs");
 
 const i = 0;
 const managerList = [];
+const engineerList = [];
+const internList = [];
 
 
+function run (){
 inquirer.prompt([{
         type: "list",
         name: "Job Title",
@@ -48,13 +51,35 @@ inquirer.prompt([{
                 manager.title = "Manager";
                 manager.email = employee.email;
                 if(managerList.length > 0){
-                    console.log("Too Many Managers");
+                    console.log("Too Many Managers Employee Not Added");
+                    inquirer.prompt([{
+                        type: "confirm",
+                        name: "recall",
+                        message: "Do you want to add another employee?"
+                    }])
+                            .then(answer => {
+                                if(answer === false){
+                                    createHTML();
+                                }else{
+                                    run();
+                                }
+                            })
                 }else{
                     managerList.push(manager);
                     console.log(managerList);
-                }
-                
-                
+                    inquirer.prompt([{
+                        type: "confirm",
+                        name: "recall",
+                        message: "Do you want to add another employee?"
+                    }])
+                            .then(answer => {
+                                if(answer.recall === false){
+                                    createHTML();
+                                }else{
+                                    run();
+                                }
+                            })
+                }                
             })
         }else if(answers["Job Title"]==="Engineer"){
             inquirer.prompt([{
@@ -67,11 +92,23 @@ inquirer.prompt([{
                 engineer.name = employee.name;
                 engineer.id = employee.id;
                 engineer.title = "Engineer"
-                engineer.email = employee.email;
-                    // $.ajax{
-                    //     engineer
-                    // }
-                
+                engineer.email = employee.email
+                engineer.giturl = `https://github.com/${engineer.github}/`
+                engineerList.push(engineer)
+                if(engineerList){
+                inquirer.prompt([{
+                    type: "confirm",
+                    name: "recall",
+                    message: "Do you want to add another employee?"
+                }])
+                        .then(answer => {
+                            if(answer.recall === false){
+                                createHTML();
+                            }else{
+                                run();
+                            }
+                        })
+                }
             })
         }else if(answers["Job Title"]==="Intern"){
             inquirer.prompt([{
@@ -85,13 +122,28 @@ inquirer.prompt([{
                 intern.id = employee.id;
                 intern.title = "Intern"
                 intern.email = employee.email;
-                // console.log(intern);
-
+                internList.push(intern);
+                if(internList){
+                inquirer.prompt([{
+                    type: "confirm",
+                    name: "recall",
+                    message: "Do you want to add another employee?"
+                }])
+                        .then(answer => {
+                            if(answer.recall === false){
+                                createHTML();
+                            }else{
+                                run();
+                            }
+                        })
+                }
             })
         }else{
             console.log("error")
         }
     })
+}
+run();
 
 // function constManager(employee){
 //         inquirer.prompt([{
@@ -160,3 +212,75 @@ inquirer.prompt([{
     //     }])
     //     .then(answers => )
     // })
+
+function createHTML(){
+        let html1 = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div id="start"></div>`
+
+        for (let i = 0 ; i < managerList.length ; i++){
+            let managerHTML = `<div class="card" style="width: 100%;">
+            <div class="card-header" id="title">${managerList[i].title}</div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item" id="name">Name: ${managerList[i].name}</li>
+              <li class="list-group-item" id="id">ID: ${managerList[i].id}</li>
+              <li class="list-group-item" id="email">Email: ${managerList[i].email}</li>
+              <li class="list-group-item" id="officeNumber">Office Number: ${managerList[i].officeNumber}</li>
+            </ul>
+          </div>`
+        html1 = html1 + managerHTML
+        }
+        for(let i = 0 ; i < engineerList.length ; i++){
+            let engineerHTML = `<div class="card" style="width: 100%;">
+            <div class="card-header" id="title">${engineerList[i].title}</div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item" id="name">Name: ${engineerList[i].name}</li>
+              <li class="list-group-item" id="id">ID: ${engineerList[i].id}</li>
+              <li class="list-group-item" id="email">Email: ${engineerList[i].email}</li>
+              <li class="list-group-item" id="github">Github: <a href="${engineerList[i].giturl}">${engineerList[i].github}</a></li>
+            </ul>
+        </div>`
+        html1 = html1 + engineerHTML
+        }
+        for(let i = 0 ; i < internList.length ; i++){
+            let internHTML = `<div class="card" style="width: 100%;">
+            <div class="card-header" id="title">${internList[i].title}</div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item" id="name">Name: ${internList[i].name}</li>
+              <li class="list-group-item" id="id">ID: ${internList[i].id}</li>
+              <li class="list-group-item" id="email">Email: ${internList[i].email}</li>
+              <li class="list-group-item" id="officeNumber">School: ${internList[i].school}</li>
+            </ul>
+          </div>`
+        html1 = html1 + internHTML
+        }
+        htmll = html1 + `<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        </body>
+        </html>`
+        inquirer.prompt([{
+            type: "confirm",
+            name: "html",
+            message: "Want to create the web profile for the team?"
+        }]).then( answer => {
+            if(answer){
+                fs.writeFile('./output/index.html', html1, (err) => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                  });
+            }else{
+                console.log("AYAYAY Our Time Is Wasted")
+            }
+        })
+    }
